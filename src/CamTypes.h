d11a2bbae615103e8fe1307407756732d33d08d0
Alexander Duda <Alexander.Duda@dfki.de>
2010-05-25 15:58:42 +0200

    added exposure mode external

--- src/CamTypes.h
+++ src/CamTypes.h
@@ -133,6 +133,7 @@ namespace camera
             ExposureModeToManual,
             ExposureModeToAuto,
             ExposureModeToAutoOnce,
+	    ExposureModeToExternal,
             MirrorXToOn,
             MirrorXToOff,
             FrameStartTriggerEventToEdgeRising,
