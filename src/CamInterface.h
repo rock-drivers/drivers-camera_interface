58b7b647894d4c373b97456b422932e78eda10a0
Alexander Duda <Alexander.Duda@dfki.de>
2010-06-07 17:35:16 +0200

    removed helper class -- all function can be found in lib FrameHelper

--- src/CamInterface.h
+++ src/CamInterface.h
@@ -423,15 +423,6 @@ namespace camera
         */
 	virtual std::string doDiagnose();
     };
-   
-    class Helper
-    {
-      public:
-      static bool convertColor(const base::samples::frame::Frame &src,
-			       base::samples::frame::Frame &dst,
-			       base::samples::frame::frame_mode_t mode= base::samples::frame::MODE_UNDEFINED);
-      static bool convertBayerToRGB24(const uint8_t *src, uint8_t *dst, int width, int height, base::samples::frame::frame_mode_t mode);
-    };
 }
 #endif	/* _CAMINTERFACE_H */
 
